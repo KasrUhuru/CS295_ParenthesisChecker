59e1462e1ebecea07dcb0db484636239505465cd
mrivera <matias.g.rivera.civ@gmail.com>
2024-08-08 15:07:49 -0700

    Modified IsBalanced to ignore non-parenthetical characters

--- CS295_ParenthesisChecker/Program.cs
+++ CS295_ParenthesisChecker/Program.cs
@@ -6,19 +6,37 @@ namespace ParenthesisChecker
 
     public class ParenthesisChecker
     {
-    static void Main(string[] args)
-    {
-        Console.Write("Should say true: ");
-        Console.WriteLine(IsBalanced("[{}]"));
-        Console.Write("Should say false: ");
-        Console.WriteLine(IsBalanced("[{}}]"));
-    }
+        static void Main(string[] args)
+        {
+            Console.Write("Empty should say true: ");
+            Console.WriteLine(IsBalanced(""));
+
+            Console.Write("[{}] Should say true: ");
+            Console.WriteLine(IsBalanced("[{}]"));
+
+            Console.Write("[{}}] Should say false: ");
+            Console.WriteLine(IsBalanced("[{}}]"));
+
+            Console.Write("[{a}}] Should say false: ");
+            Console.WriteLine(IsBalanced("[{a}}]"));
+
+            // It works! Ignores non-parenthesis characters without trouble
+            Console.Write("Dictionary<string, Room> Exits = new Dictionary<string, Room>() Should say true: ");
+            Console.WriteLine(IsBalanced("Dictionary<string, Room> Exits = new Dictionary<string, Room>()"));
+
+        }
+
         public static bool IsBalanced(string expression)
         {
             Stack<char> stack = new Stack<char>();
 
             foreach (char c in expression)
             {
+                // Empty strings are balanced
+                if (c == null) { return true; }
+                // Ignore non-parenthetical characters without returning false for real-world utility
+                if (c != '(' || c != '[' || c != '{' || c != ')' || c != ']' || c != '}') { continue; }
+                // 
                 if (c == '(' || c == '[' || c == '{')
                 {
                     stack.Push(c);
@@ -30,10 +48,10 @@ namespace ParenthesisChecker
                         return false;
                     }
 
-                    char top = stack.Pop();
-                    if ((c == ')' && top != '(') ||
-                        (c == ']' && top != '[') ||
-                        (c == '}' && top != '{'))
+                    char lastIn = stack.Pop();
+                    if ((c == ')' && lastIn != '(') ||
+                        (c == ']' && lastIn != '[') ||
+                        (c == '}' && lastIn != '{'))
                     {
                         return false;
                     }
